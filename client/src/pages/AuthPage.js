c878384ec730346ffe472c5f7b0261c57793b35a
Nicorvenius <nicorvenius@gmail.com>
2020-04-27 16:32:13 +0300

    Update first page

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -64,84 +64,121 @@ export default class AuthPage extends Component {
                 <Carousel>
                     <div className="carousel-father">
                         <img
-                            src="https://images.unsplash.com/photo-1557264322-b44d383a2906?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjI0MX0&auto=format&fit=crop&w=1950&q=80"
+                            src="/api/photo/carousel_1"
                             alt=""/>
                     </div>
                     <div className="carousel-father">
                         <img
-                            src="https://images.unsplash.com/photo-1512805668868-1608a189cc2b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format"
+                            src="/api/photo/carousel_2"
                             alt=""/>
                     </div>
                     <div className="carousel-father">
                         <img
-                            src="https://images.unsplash.com/photo-1512805668868-1608a189cc2b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format"
+                            src="/api/photo/carousel_3"
                             alt=""/>
                     </div>
                     <div className="carousel-father">
                         <img
-                            src="https://images.unsplash.com/photo-1512805668868-1608a189cc2b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format"
+                            src="/api/photo/carousel_4"
                             alt=""/>
                     </div>
                 </Carousel>
                 <div className="block-background">
                     <Row justify="center">
-                        <Col className="gutter-row" span={4}>
+                        <Col className="gutter-row" span={20}>
                             <img
-                                src="https://images.unsplash.com/photo-1572294459454-ffe72ebf382a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1575&q=80"
-                                alt=""/>
-                        </Col>
-                        <Col className="gutter-row" span={12}>
-                            <div className="col s8">
-                                <div className="block-text">
-                                    <h3 className="block-text__title">Textbooks from the Noba Collection.</h3>
-
-                                    <p className="block-text__description">Expertly compiled from Noba modules to fit
-                                        the scope and sequence of common courses. Use them as-is or customize them to
-                                        fit your needs. Instructor manual, PowerPoint presentations, and test bank
-                                        available for many modules.</p>
-                                </div>
-                            </div>
-                        </Col>
-                    </Row>
-                </div>
-                <div className="block">
-                    <Row justify="center">
-                        <Col className="gutter-row" span={12}>
-                            <div className="block-text">
-                                <h3 className="block-text__title">Textbooks from the Noba Collection.</h3>
-
-                                <p className="block-text__description">Expertly compiled from Noba modules to fit the
-                                    scope and sequence of common courses. Use them as-is or customize them to fit your
-                                    needs. Instructor manual, PowerPoint presentations, and test bank available for many
-                                    modules.</p>
-                            </div>
-                        </Col>
-                        <Col className="gutter-row" span={4}>
-                            <img
-                                src="https://images.unsplash.com/photo-1572294459454-ffe72ebf382a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1575&q=80"
+                                src="/api/photo/image_2"
                                 alt=""/>
                         </Col>
                     </Row>
                 </div>
-                <div className="block">
+                <div className="block-background">
                     <Row justify="center">
-                        <Col className="gutter-row" justify="center" span={2} style={{padding: '10px'}}>
-                            <Progress type="circle" percent={75} style={styleJustifyCenter}/>
-                            <div className="col s4" style={styleJustifyCenter}>123</div>
-                        </Col>
-                        <Col className="gutter-row" justify="center" span={2} style={{padding: '10px'}}>
-                            <Progress type="circle" percent={70} status="exception" style={styleJustifyCenter}/>
-                            <div className="col s4" style={styleJustifyCenter}>123</div>
-                        </Col>
-                        <Col className="gutter-row" justify="center" span={2} style={{padding: '10px'}}>
-                            <Progress type="circle" style={styleJustifyCenter} percent={100}/>
-                            <div className="col s4" style={styleJustifyCenter}>123</div>
+                        <Col className="gutter-row" span={20}>
+                            <h3 className="title-section">Правила курса</h3>
                         </Col>
 
+                        <Col className="gutter-row" span={10} style={{margin: '20px'}}>
+                            <img src="/api/photo/book" alt="" className="section__image"/>
+                            <h5 className="section-item__title">ТЕОРИЯ</h5>
+                            <p className="section-item__description">Мы преподаем исключительно тот, материал, который актуален <br/> и поможет разобраться с практическими заданиями.</p>
+                        </Col>
+                        <Col className="gutter-row" span={10} style={{margin: '20px'}}>
+                            <img src="/api/photo/graduation" alt="" className="section__image"/>
+                            <h5 className="section-item__title">ПРАКТИКА</h5>
+                            <p className="section-item__description">60% всего времени на курсах это практические занятия. <br/> Мы верим в правило трех «П»: практика, практика и еще раз практика.</p>
+                        </Col>
+                        <Col className="gutter-row" span={10} style={{margin: '20px'}}>
+                            <img src="/api/photo/rocket" alt="" className="section__image"/>
+                            <h5 className="section-item__title">ЭФФЕКТИВНОЕ ОБУЧЕНИЕ</h5>
+                            <p className="section-item__description">Современная информационная индустрия с важными деталями обучения.</p>
+                        </Col>
+                        <Col className="gutter-row" span={10} style={{margin: '20px'}}>
+                            <img src="/api/photo/education" alt="" className="section__image"/>
+                            <h5 className="section-item__title">ОТВЕТСТВЕННОСТЬ</h5>
+                            <p className="section-item__description">Ваш успех зависит от ваших собственных решений.</p>
+                        </Col>
                     </Row>
                 </div>
+                {/*<div className="block-background">*/}
+                {/*    <Row justify="center">*/}
+                {/*        <Col className="gutter-row" span={4}>*/}
+                {/*            <img*/}
+                {/*                src="https://images.unsplash.com/photo-1572294459454-ffe72ebf382a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1575&q=80"*/}
+                {/*                alt=""/>*/}
+                {/*        </Col>*/}
+                {/*        <Col className="gutter-row" span={12}>*/}
+                {/*            <div className="col s8">*/}
+                {/*                <div className="block-text">*/}
+                {/*                    <h3 className="block-text__title">Textbooks from the Noba Collection.</h3>*/}
+
+                {/*                    <p className="block-text__description">Expertly compiled from Noba modules to fit*/}
+                {/*                        the scope and sequence of common courses. Use them as-is or customize them to*/}
+                {/*                        fit your needs. Instructor manual, PowerPoint presentations, and test bank*/}
+                {/*                        available for many modules.</p>*/}
+                {/*                </div>*/}
+                {/*            </div>*/}
+                {/*        </Col>*/}
+                {/*    </Row>*/}
+                {/*</div>*/}
+                {/*<div className="block">*/}
+                {/*    <Row justify="center">*/}
+                {/*        <Col className="gutter-row" span={12}>*/}
+                {/*            <div className="block-text">*/}
+                {/*                <h3 className="block-text__title">Textbooks from the Noba Collection.</h3>*/}
+
+                {/*                <p className="block-text__description">Expertly compiled from Noba modules to fit the*/}
+                {/*                    scope and sequence of common courses. Use them as-is or customize them to fit your*/}
+                {/*                    needs. Instructor manual, PowerPoint presentations, and test bank available for many*/}
+                {/*                    modules.</p>*/}
+                {/*            </div>*/}
+                {/*        </Col>*/}
+                {/*        <Col className="gutter-row" span={4}>*/}
+                {/*            <img*/}
+                {/*                src="https://images.unsplash.com/photo-1572294459454-ffe72ebf382a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1575&q=80"*/}
+                {/*                alt=""/>*/}
+                {/*        </Col>*/}
+                {/*    </Row>*/}
+                {/*</div>*/}
+                {/*<div className="block">*/}
+                {/*    <Row justify="center">*/}
+                {/*        <Col className="gutter-row" justify="center" span={2} style={{padding: '10px'}}>*/}
+                {/*            <Progress type="circle" percent={75} style={styleJustifyCenter}/>*/}
+                {/*            <div className="col s4" style={styleJustifyCenter}>123</div>*/}
+                {/*        </Col>*/}
+                {/*        <Col className="gutter-row" justify="center" span={2} style={{padding: '10px'}}>*/}
+                {/*            <Progress type="circle" percent={70} status="exception" style={styleJustifyCenter}/>*/}
+                {/*            <div className="col s4" style={styleJustifyCenter}>123</div>*/}
+                {/*        </Col>*/}
+                {/*        <Col className="gutter-row" justify="center" span={2} style={{padding: '10px'}}>*/}
+                {/*            <Progress type="circle" style={styleJustifyCenter} percent={100}/>*/}
+                {/*            <div className="col s4" style={styleJustifyCenter}>123</div>*/}
+                {/*        </Col>*/}
+
+                {/*    </Row>*/}
+                {/*</div>*/}
                 <div className="course">
-                    <Row justify="center">т
+                    <Row justify="center">
                         { this.state.list.map((item, index) => {
                             let photoUrl = "https://images.unsplash.com/photo-1566193978446-fa48cfe7579d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80"
                             if (item.photo !== ''){
