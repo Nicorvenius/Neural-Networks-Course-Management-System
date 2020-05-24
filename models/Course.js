28f4fde9d93feec3660f4365216caef490f8db02
Nicorvenius <nicorvenius@gmail.com>
2020-05-25 01:33:25 +0300

    Init the Course schema for CreateCourse page

--- models/Course.js
+++ models/Course.js
@@ -4,8 +4,11 @@ const {Schema, model, Types} = require('mongoose');
 const schema = new Schema({
     title:{type: String, default: ""},
     content:{type: String, default: ""},
+    description:{type: String, default: ""},
     Date:{type: Date, default: Date.now},
     photo: {type: String, required: false, default: ''},
+    section_image: {type: String, required: false, default: ''},
+    banner: {type: String, required: false, default: ''},
     userId:{type: Schema.Types.ObjectId, ref: 'User'},
 })
 
